a733a3d9d03fe4a17376fa179f2b692e2006026b
Matteo Pieroni <matteopieroni6@gmail.com>
2020-10-10 19:28:26 +0100

    [Feature] Remove dialog for GMs when there are active owners of a character (#2)
    * Added a check for non-GM owners before showing dialog to GM
    
    * Added check to see if other owners are active

---
+++ src/user-identity/user-identity.js
@@ -0,0 +1,44 @@
+function checkHasOwnerOtherThanGm(targetId) {
+	const users = game?.users;
+	const actor = game?.actors?.get(targetId);
+
+	// no-op
+	if (!actor) {
+		return false;
+	}
+
+	// if the current user is the GM, remove GM and default properties and check other permissions
+	const nonGMPermissionUsers = Object.keys(actor.data.permission)
+		.filter(perm => perm !== game.userId && perm !== 'default')
+		.filter(perm => actor.data.permission[perm] === 3);
+
+	if (nonGMPermissionUsers.length === 0) {
+		return false;
+	}
+
+	// check any remaining owners are active
+	const hasNonGMPermissionInGame = nonGMPermissionUsers
+		.filter(permUser => users.get(permUser).active)
+		.length > 0;
+
+
+	// if the current user is the GM and
+	// if there are other non-GM users with owner permission
+	return hasNonGMPermissionInGame;
+}
+
+export function checkShouldShowDialog(target) {
+	const targetId = target?.actor?.id;
+	const users = game?.users;
+	const currentUser = users?.get(game.userId);
+
+	if (!targetId || !currentUser) {
+		return true;
+	}
+
+	if (currentUser.isGM && checkHasOwnerOtherThanGm(targetId)) {
+		return false;
+	}
+
+	return true;
+}
