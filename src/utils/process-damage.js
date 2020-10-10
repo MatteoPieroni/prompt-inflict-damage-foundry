f2d8ee78d72e3b7cc79dde08cb7c6abe6cd8b1fc
Matteo Pieroni <matteopieroni6@gmail.com>
2020-10-10 17:14:22 +0100

    [Bugfix] Changed calc function to directly access actor (#1)
    * Moved to reference to actor
    
    * Added dev build
    
    * Moved console.logs out of production

--- src/utils/process-damage.js
+++ src/utils/process-damage.js
@@ -1,4 +1,6 @@
-export function processDamage({ hp, damage, isHalved, modifier }) {
+// we pass the actor instance here, because passing hp directly caused the value
+// to be in the closure and not up to date
+export function processDamage({ actorInstance, damage, isHalved, modifier }) {
 	const initialDamage = isHalved ? Math.floor(damage / 2) : damage;
 	const trimmedModifier = typeof modifier === 'string' && modifier.replaceAll(' ', '');
 	const finalModifiers = trimmedModifier && trimmedModifier.split(/(?<=[+-][0-9]+)/i);
@@ -12,7 +14,10 @@ export function processDamage({ hp, damage, isHalved, modifier }) {
 	}
 
 	const hasDamage = modifiedDamage > 0;
-	const newCalculatedHp = hp - modifiedDamage > 0 ? hp - modifiedDamage : 0;
+	const newCalculatedHp = 
+		actorInstance.data.data.attributes.hp.value - modifiedDamage > 0 ?
+		actorInstance.data.data.attributes.hp.value - modifiedDamage :
+		0;
 
 	return {
 		damage: hasDamage ? modifiedDamage : null,
