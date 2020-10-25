c43cb8c580d514884c6cbc8c0f13630fdeb0ae08
Matteo Pieroni <matteopieroni6@gmail.com>
2020-10-25 16:29:43 +0000

    [Feature] Support AiME system (#5)
    * Added check for flag related to dnd5e or aime

--- src/inflict-damage/inflict-damage.js
+++ src/inflict-damage/inflict-damage.js
@@ -2,7 +2,7 @@ import { showInfo } from '../dialog/dialog';
 import { log } from '../utils';
 
 export async function inflictDamage({ app = {}, data = {}, }) {
-	const type = app?.data?.flags?.dnd5e?.roll?.type;
+	const type = app?.data?.flags?.dnd5e?.roll?.type || app?.data?.flags?.aime?.roll?.type;
 	const targets = app.user && app.user.targets;
 
 	const isDamage = type === 'damage';
