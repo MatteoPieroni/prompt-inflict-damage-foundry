b714863f27dab5643748f0bdc1dce1563a327c5d
Matteo Pieroni <matteopieroni6@gmail.com>
2020-11-17 18:26:20 +0000

    [Feature] Setting based flag (#6)
    * Added game setting for flag
    
    * Moved to registered flag to support more systems
    
    * Fallback on system id

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ let isReady = false;
 
 Hooks.on('ready', () => {
 	isReady = true;
+
+	/**
+	 * The domain on which the flag will be looked up
+	 */
+	game.settings.register("prompt-inflict-damage", "flagDomain", {
+		name : game.i18n.localize('PID.SettingFlagName'),
+		hint : game.i18n.localize('PID.SettingFlagNameHint'),
+		scope : 'world',
+		config : true,
+		type : String,
+		default : game.system.id,
+	});
+	
 });
 
 Hooks.on("renderChatMessage", (app, html, data) => {
